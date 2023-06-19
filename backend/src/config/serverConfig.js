44736d40ff841205617a082bbce2a735f6477f84
Tarun Nahak <tarunnahak25@gmail.com>
2023-06-19 16:09:06 +0530

    Added signup

--- backend/src/config/serverConfig.js
+++ backend/src/config/serverConfig.js
@@ -5,4 +5,5 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT,
   MONGODB_URI: process.env.MONGODB_URI,
+  SALT: process.env.SALT,
 };
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,42 @@
-const signup = (req, res) => {};
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user-model");
+const { SALT } = require("../config/serverConfig");
+const signup = (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username }).then((user) => {
+    if (user) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    const newUser = new User({
+      username,
+      password,
+    });
+
+    // Hash password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+
+        // Save user to the database
+        newUser
+          .save()
+          .then(() =>
+            res
+              .status(201)
+              .json({ message: "User created successfully", data: newUser })
+          )
+          .catch((err) =>
+            res.status(500).json({ message: "Error creating user", error: err })
+          );
+      });
+    });
+  });
+};
 
 const login = (req, res, next) => {};
 
